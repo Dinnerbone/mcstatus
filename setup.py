8d71cefeb07768fdbe8ad624cbb0c7a63d98bc7b
Kevin Tindall <kevinkjt2000@gmail.com>
2020-08-06 09:16:00 -0500

    Bump major rev

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ tests_require = [
 
 setup(
     name='mcstatus',
-    version='3.1.1',
+    version='4.0.0',
     author='Nathan Adams',
     author_email='dinnerbone@dinnerbone.com',
     url='https://pypi.python.org/pypi/mcstatus',
