5320c870c59cf1d52d8e986f0da2e8a8b55aa4e5
Kevin Tindall <kevinkjt2000@gmail.com>
2021-06-15 11:40:21 -0500

    Minor version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("test-requirements.txt") as f:
 
 setup(
     name="mcstatus",
-    version="6.0.0",
+    version="6.1.0",
     author="Nathan Adams",
     author_email="dinnerbone@dinnerbone.com",
     url="https://pypi.python.org/pypi/mcstatus",
