8b84e4b0b8aac04a49a955fd7a4306f6d4d36752
Kevin Tindall <kevinkjt2000@gmail.com>
2021-07-04 22:48:49 -0500

    Feature bump for #151

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("test-requirements.txt") as f:
 
 setup(
     name="mcstatus",
-    version="6.1.2",
+    version="6.2.0",
     author="Nathan Adams",
     author_email="dinnerbone@dinnerbone.com",
     url="https://pypi.python.org/pypi/mcstatus",
