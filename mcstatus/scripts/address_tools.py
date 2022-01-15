53967d00783bb9ec011e6f7440ac67a126694f5e
ItsDrike <itsdrike@protonmail.com>
2022-01-16 00:07:13 +0100

    Add return type annotations in address_tools functions

--- mcstatus/scripts/address_tools.py
+++ mcstatus/scripts/address_tools.py
@@ -1,15 +1,16 @@
 from urllib.parse import urlparse
+from typing import Tuple, Optional, Union
 from ipaddress import ip_address
 
 
-def ip_type(address):
+def ip_type(address: Union[int, str]) -> Optional[int]:
     try:
         return ip_address(address).version
     except ValueError:
         return None
 
 
-def parse_address(address):
+def parse_address(address: str) -> Tuple[str, Optional[int]]:
     tmp = urlparse("//" + address)
     if not tmp.hostname:
         raise ValueError(f"Invalid address '{address}'")
